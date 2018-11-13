2f6eff6bfba9170dbf1206d21b063369839116b9
xshaitt <xshaitt@gmail.com>
2018-11-13 12:50:37 +0800

    first commit

---
+++ app/tasks/MainTask.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xshaitt
+ * Date: 2018/11/13
+ * Time: 下午12:09
+ */
+
+use Phalcon\Cli\Task;
+
+class MainTask extends Task
+{
+    public function mainAction()
+    {
+        echo 'This is the default task and the default action' . PHP_EOL;
+    }
+
+    public function testAction(array $params)
+    {
+        echo sprintf('hello %s', $params[0]);
+
+        echo PHP_EOL;
+
+        echo sprintf('best regards, %s', $params[1]);
+
+        echo PHP_EOL;
+    }
+}
